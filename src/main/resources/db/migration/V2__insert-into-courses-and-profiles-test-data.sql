3f6c6bfdfee46ca965d328ea36d401afcd117993
PatoProgramador <skpates18@gmail.com>
2024-07-11 21:19:27 -0500

    Test data for topics

---
+++ src/main/resources/db/migration/V2__insert-into-courses-and-profiles-test-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO courses(name, category) VALUES ("Spring boot:crea tu api", "Backend");
+INSERT INTO courses(name, category) VALUES ("Java básico", "Backend");
+INSERT INTO courses(name, category) VALUES ("Java intermedio", "Backend");
+INSERT INTO courses(name, category) VALUES ("Programacion orientada a objetos", "Backend");
+
+INSERT INTO profiles(name, email) VALUES ("Sofia Villadiego", "laSofis@gmail.com");
+INSERT INTO profiles(name, email) VALUES ("Victoria España", "laVicky@gmail.com");
+INSERT INTO profiles(name, email) VALUES ("Spike Spiegel", "elCowboy@gmail.com");
